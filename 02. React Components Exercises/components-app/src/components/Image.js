40def07a54a15c3432f8d279bc899dae8acf5eb2
Jordan Jordanov <d_yugioh@abv.bg>
2017-11-04 10:03:20 +0200

    React components Exercises

---
+++ 02. React Components Exercises/components-app/src/components/Image.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+let Image = (props) => {
+  return (
+    <img src={props.url} alt="img" className='char-img'/>
+  )
+}
+
+export default Image
---
+++ 02. React Components Exercises/components-app/src/utils/observer.js
@@ -0,0 +1,13 @@
+let funcHolder = {}
+
+let observerMenu = {
+  addObserver: (name, func) => {
+    funcHolder[name] = func
+  },
+
+  executeObserver: (name, param) => {
+    funcHolder[name](param)
+  }
+}
+
+export default observerMenu
