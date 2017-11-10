9c0802edb0e8fb3f8b51ad314803bca894da9f4c
Jordan Jordanov <d_yugioh@abv.bg>
2017-11-10 12:56:03 +0200

    04. Routing and Architecture Exercises
    04. Routing and Architecture Exercises

---
+++ seenit/src/components/common/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+let Footer = () => {
+  return <footer>SeenIt SPA © 2017 JJ</footer>;
+}
+
+export default Footer;
---
+++ seenit/src/components/posts/Submit.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import reqHandler from './../../utils/reqHandler'
+import notifiy from './../../notifications/notify'
+import { Redirect } from 'react-router-dom'
+
+class Submit extends Component {
+
+  onChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  createPost = (e) => {
+    e.preventDefault()
+    if (this.state === null) {
+      notifiy.showError('Please fill all the required fields')
+      return
+    }
+    let payload = {
+      author: localStorage.getItem('username'),
+      title: this.state.title,
+      description: this.state.description,
+      url: this.state.url,
+      imageUrl: this.state.image
+    }
+
+    reqHandler.createPost(payload)
+      .then(response => {
+        notifiy.showInfo('Post created!')
+        window.location.replace('/catalog')
+      })
+  }
+
+  render () {
+    return (
+      <section id="viewSubmit">
+        <div className="submitArea">
+          <h1>Submit Link</h1>
+          <p>Please, fill out the form. A thumbnail image is not required.</p>
+        </div>
+        <div className="submitArea formContainer">
+          <form id="submitForm" className="submitForm" onSubmit={this.createPost}>
+            <label>Link URL:</label>
+            <input onChange={this.onChange} name="url" type="text"/>
+            <label>Link Title:</label>
+            <input onChange={this.onChange} name="title" type="text"/>
+            <label>Link Thumbnail Image (optional):</label>
+            <input onChange={this.onChange} name="image" type="text"/>
+            <label>Description (optional):</label>
+            <textarea onChange={this.onChange} name="description"></textarea>
+            <input id="btnSubmitPost" value="Submit" type="submit"/>
+          </form>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Submit
